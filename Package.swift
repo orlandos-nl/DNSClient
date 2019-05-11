3f9ed96afab3878d09737e18556110e528135099
Joannis Orlandos <joannis@orlandos.nl>
2019-05-11 18:18:53 +0200

    Implement for Swift 5 only

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:4.2
+// swift-tools-version:5.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -23,7 +23,7 @@ let package = Package(
         .target(
             name: "NioDNS",
             dependencies: ["NIO", "CResolvHelpers"]),
-        .target(name: "CResolvHelpers"),
+        .target(name: "CResolvHelpers", linkerSettings: [LinkerSetting.linkedLibrary("resolv")]),
         .testTarget(
             name: "NioDNSTests",
             dependencies: ["NioDNS", "NIO"]),
---
+++ Sources/NioDNS/Messages/MessageOptions.swift
@@ -0,0 +1,88 @@
+fileprivate let opCodeBits: UInt16 = 0b01111000_00000000
+fileprivate let resultCodeBits: UInt16 = 0b00000000_00001111
+
+public struct MessageOptions: OptionSet, ExpressibleByIntegerLiteral {
+    public var rawValue: UInt16
+
+    public init(rawValue: UInt16) {
+        self.rawValue = rawValue
+    }
+
+    public init(integerLiteral value: UInt16) {
+        self.rawValue = value
+    }
+
+    public static let answer: MessageOptions = 0b10000000_00000000
+    public static let authorativeAnswer: MessageOptions = 0b00000100_00000000
+    public static let truncated: MessageOptions = 0b00000010_00000000
+    public static let recursionDesired: MessageOptions = 0b00000001_00000000
+    public static let recursionAvailable: MessageOptions = 0b00000000_10000000
+
+    public static let standardQuery: MessageOptions = 0b00000000_00000000
+    public static let inverseQuery: MessageOptions = 0b00001000_00000000
+    public static let serverStatusQuery: MessageOptions = 0b00010000_00000000
+
+    public static let resultCodeSuccess: MessageOptions = 0b00000000_00000000
+    public static let resultCodeFormatError: MessageOptions = 0b00000000_00000001
+    public static let resultCodeServerfailure: MessageOptions = 0b00000000_00000010
+    public static let resultCodeNameError: MessageOptions = 0b00000000_00000011
+    public static let resultCodeNotImplemented: MessageOptions = 0b00000000_00000100
+    public static let resultCodeNotRefused: MessageOptions = 0b00000000_00000101
+
+    public var isAnswer: Bool {
+        return self.contains(.answer)
+    }
+
+    public var isAuthorativeAnswer: Bool {
+        return self.contains(.authorativeAnswer)
+    }
+
+    public var isQuestion: Bool {
+        return !isAnswer
+    }
+
+    public var isStandardQuery: Bool {
+        return rawValue & opCodeBits == MessageOptions.standardQuery.rawValue
+    }
+
+    public var isInverseQuery: Bool {
+        return rawValue & opCodeBits == MessageOptions.inverseQuery.rawValue
+    }
+
+    public var isServerStatusQuery: Bool {
+        return rawValue & opCodeBits == MessageOptions.serverStatusQuery.rawValue
+    }
+
+    public var isSuccessful: Bool {
+        return self.contains(.resultCodeSuccess)
+    }
+
+    public var isFormatError: Bool {
+        return self.contains(.resultCodeFormatError)
+    }
+
+    public var isServerFailure: Bool {
+        return self.contains(.resultCodeServerfailure)
+    }
+
+    public var isNameError: Bool {
+        return self.contains(.resultCodeNameError)
+    }
+
+    public var isNotImplemented: Bool {
+        return self.contains(.resultCodeNotImplemented)
+    }
+
+    public var isNotRefused: Bool {
+        return self.contains(.resultCodeNotRefused)
+    }
+
+    public var isRefused: Bool {
+        return !isNotRefused
+    }
+
+    //    Note that the contents of the answer section may have
+    //    multiple owner names because of aliases.
+    //    The AA bit corresponds to the name which matches the query name, or
+    //    the first owner name in the answer section.
+}
