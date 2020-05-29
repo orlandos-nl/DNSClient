66bbe9d2ee2bb977d6788924dcbcab72150a45aa
Joannis Orlandos <joannis@orlandos.nl>
2020-05-29 14:16:28 +0200

    Support new iOS platforms

--- Package.swift
+++ Package.swift
@@ -1,10 +1,14 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.2
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "DNSClient",
+    platforms: [
+        .macOS(.v10_15),
+        .iOS(.v13),
+    ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
