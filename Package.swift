8a7b1be0208e6e9d1d9e0a07713e0c0ebd7a2010
Joannis Orlandos <joannis@orlandos.nl>
2023-03-27 10:20:43 +0200

    Add NIOTransportServices to NIODNS

--- Package.swift
+++ Package.swift
@@ -9,21 +9,30 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "DNSClient",
-            targets: ["DNSClient"]),
+            targets: [
+                "DNSClient"
+            ]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/apple/swift-nio.git", from: "2.0.0"),
+        .package(url: "https://github.com/apple/swift-nio-transport-services.git", from: "1.15.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .testTarget(
             name: "DNSClientTests",
-            dependencies: ["DNSClient", "NIO"]),
+            dependencies: [
+                .target(name: "DNSClient"),
+                .product(name: "NIO", package: "swift-nio"),
+            ]),
         .target(
             name: "DNSClient",
-            dependencies: ["NIO"]
+            dependencies: [
+                .product(name: "NIOTransportServices", package: "swift-nio-transport-services"),
+                .product(name: "NIO", package: "swift-nio"),
+            ]
         )
     ]
 )
