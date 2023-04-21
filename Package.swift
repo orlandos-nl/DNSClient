092077c4d7aa5b2e7521f4e07201ccd7f7cf0cb5
Joannis Orlandos <joannis@orlandos.nl>
2023-04-21 15:53:04 +0200

    Rely on custom NIOTS

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.5
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -16,7 +16,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/apple/swift-nio.git", from: "2.0.0"),
-        .package(url: "https://github.com/apple/swift-nio-transport-services.git", from: "1.15.0")
+        .package(url: "https://github.com/Joannis/swift-nio-transport-services.git", revision: "feature/udp-networking-framework-support"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
