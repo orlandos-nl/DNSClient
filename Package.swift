e857edc37bc6451a9f4426254899232fe7247e69
Orhan Yigit Yazicilar <y_yazicilar@apple.com>
2025-08-28 10:19:35 +0100

    build: update to Swift 5.10 and update dependencies

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.5
+// swift-tools-version:5.10
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -15,12 +15,13 @@ let package = Package(
             name: "DNSClient",
             targets: [
                 "DNSClient"
-            ]),
+            ]
+        )
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/apple/swift-nio.git", from: "2.0.0"),
-        .package(url: "https://github.com/apple/swift-nio-transport-services.git", from: "1.19.0"),
+        .package(url: "https://github.com/apple/swift-nio.git", from: "2.85.0"),
+        .package(url: "https://github.com/apple/swift-nio-transport-services.git", from: "1.25.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -30,13 +31,14 @@ let package = Package(
             dependencies: [
                 .target(name: "DNSClient"),
                 .product(name: "NIO", package: "swift-nio"),
-            ]),
+            ]
+        ),
         .target(
             name: "DNSClient",
             dependencies: [
                 .product(name: "NIOTransportServices", package: "swift-nio-transport-services"),
                 .product(name: "NIO", package: "swift-nio"),
             ]
-        )
+        ),
     ]
 )
