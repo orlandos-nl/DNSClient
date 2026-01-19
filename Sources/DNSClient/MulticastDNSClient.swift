fdd3eb3e022b5db172e65723c1ffe565118161a5
Joannis Orlandos <joannis@orlandos.nl>
2026-01-19 13:47:30 +0100

    Update multicast on Windows

--- Sources/DNSClient/MulticastDNSClient.swift
+++ Sources/DNSClient/MulticastDNSClient.swift
@@ -19,8 +19,10 @@ public final class MulticastDNSClient: DNSClient, @unchecked Sendable {
         let dnsDecoder = DNSDecoder(group: group)
 
         let bootstrap = DatagramBootstrap(group: group)
+            #if !os(Windows)
             .channelOption(ChannelOptions.socket(SocketOptionLevel(SOL_SOCKET), SO_REUSEADDR), value: 1)
             .channelOption(ChannelOptions.socket(SocketOptionLevel(SOL_SOCKET), SO_REUSEPORT), value: 1)
+            #endif
             .channelInitializer { channel in
                 return channel.pipeline.addHandlers(
                     EnvelopeInboundChannel(),
@@ -30,7 +32,7 @@ public final class MulticastDNSClient: DNSClient, @unchecked Sendable {
                 )
             }
 
-        let ipv4 = address.protocol.rawValue == PF_INET
+        let ipv4 = address.protocol == .inet
 
         return bootstrap.bind(host: ipv4 ? "0.0.0.0" : "::", port: 0).map { channel in
             let client = MulticastDNSClient(
